734c8558afee48e0ad949ae880f81903b92ad8d0
hyungjin099 <hyungjin099@naver.com>
2024-05-31 12:42:18 +0900

    1

---
+++ workspace_java/JavaBasic/src/세번째_연산자/연산자_1.java
@@ -0,0 +1,34 @@
+package 세번째_연산자;
+
+public class 연산자_1 {
+    public static void main(String[] args) {
+        //산술 연산자 : +, -, * , /, % (나머지)
+        // % mod 연산자
+        System.out.println(10 / 4); //2
+        System.out.println(10 % 4); //2
+        System.out.println(10 % 3); //1
+
+        System.out.println(+10);
+        System.out.println(-10);
+
+        //비교 연산자
+        // == 같다  ,  != 같지 않다.
+        // > , >=, <, <=
+        System.out.println(5 >= 3);
+
+        //논리 연산자(맞다(참), 틀리다(거짓))
+        // x가 5보다 크고 10이하다 ->  5 < x <= 10
+        // && 그리고, || 그러나
+        // & 그리고, | 그러나
+        int x = 3;
+        boolean r1 = x > 5 && x <= 10;
+
+
+    }
+}
+
+
+
+
+
+
